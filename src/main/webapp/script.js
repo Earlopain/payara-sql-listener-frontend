24ff1faf00b107975a995896f1d46ed007391e2d
Earlopain <earlopain@c5h8no4na.net>
2021-02-18 11:21:37 +0100

    Add a toast on lib actions activated/deactivated/cleared

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,16 +1,28 @@
 class WebsocketHandler {
 	constructor() {
 		this.ws = new WebSocket("ws://" + window.location.host + window.location.pathname + "websocket");
-		this.ws.addEventListener("message", this.handleIncoming);
+		this.ws.addEventListener("message", event => { this.handleIncoming(event) });
+		this.toast = new bootstrap.Toast(document.querySelector('.toast'));
 	}
 
 	handleIncoming(event) {
-		let messageArea = document.getElementById("messages");
-		messageArea.value += event.data + "\r\n";
+		const json = JSON.parse(event.data);
+		if (json.type === "STATUSREPORT") {
+			const message = json.message;
+			this.createToast(message);
+		} else {
+			let messageArea = document.getElementById("messages");
+			messageArea.value += event.data + "\r\n";
+		}
 	}
 
 	sendMessage(message) {
 		this.ws.send(message);
+	}
+
+	createToast(message) {
+		this.toast._element.querySelector(".toast-body").innerText = message;
+		this.toast.show();
 	}
 }
 
